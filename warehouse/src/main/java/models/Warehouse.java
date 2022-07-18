1dcba9afc14477c8ad652285720a0006d2863566
Atul Mishra <atulabhishek3@gmail.com>
2022-07-18 14:54:02 -0400

    minor changes to initial commit

--- warehouse/src/main/java/models/Warehouse.java
+++ warehouse/src/main/java/models/Warehouse.java
@@ -1,5 +1,32 @@
 package models;
 
 public class Warehouse {
-
+	private int WarehouseId;
+	private String WarehouseName;
+	public int getWarehouseId() {
+		return WarehouseId;
+	}
+	public void setWarehouseId(int warehouseId) {
+		WarehouseId = warehouseId;
+	}
+	public String getWarehouseName() {
+		return WarehouseName;
+	}
+	public void setWarehouseName(String warehouseName) {
+		WarehouseName = warehouseName;
+	}
+	public Warehouse() {
+		super();
+	}
+	public Warehouse(int warehouseId, String warehouseName) {
+		super();
+		WarehouseId = warehouseId;
+		WarehouseName = warehouseName;
+	}
+	@Override
+	public String toString() {
+		return "Warehouse [WarehouseId=" + WarehouseId + ", WarehouseName=" + WarehouseName + "]";
+	}
+	
+	
 }
